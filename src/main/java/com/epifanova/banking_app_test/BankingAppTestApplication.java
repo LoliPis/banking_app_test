4403cb8f9b34cb5c35f8adafc0de3149b5b8a7e3
Valeria Epifanova <epifanova.valera@yandex.ru>
2023-11-09 09:24:46 +0300

    Initial commit

---
+++ src/main/java/com/epifanova/banking_app_test/BankingAppTestApplication.java
@@ -0,0 +1,13 @@
+package com.epifanova.banking_app_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingAppTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingAppTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epifanova/banking_app_test/BankingAppTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epifanova.banking_app_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
